7c384f59e0d3e36b494bfbf1c025f49cb7590900
Herbertholzkopf <93044485+Herbertholzkopf@users.noreply.github.com>
2025-10-15 17:55:01 +0200

    v35

--- index.php
+++ index.php
@@ -1083,7 +1083,7 @@ $dashboardData = array_values($dashboardData);
 
 <footer class="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 py-4 z-10">
     <div class="container mx-auto text-center">
-        Made with ❤️ by <a href="https://github.com/Herbertholzkopf/" class="footer-link">Andreas Koller - 43h Arbeitszeit (Stand 14.10.2025)</a>
+        Made with ❤️ by <a href="https://github.com/Herbertholzkopf/" class="footer-link">Andreas Koller - 43h Arbeitszeit (Stand 15.10.2025)</a>
     </div>
 </footer>
 
